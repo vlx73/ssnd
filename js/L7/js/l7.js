60fe3cd02c44b6d1a4241ba9ce878ea71906bee9
Vlado Mečiar <vmeciar@buttonsystems.eu>
2024-01-20 14:06:30 +0100

    Vyčistené príklady na demonštráciu eventListenera a event bubling.

--- js/L7/js/l7.js
+++ js/L7/js/l7.js
@@ -1,38 +1,10 @@
-// document.addEventListener('readystatechange', (event) => {
-//     console.log('readystatechange event',event);
-//     console.log(event.target.readyState);
-
-//     if(event.target.readyState === 'complete'){
-//         initApp();
-//     }
-// })
-
-// const initApp = () => {
-// const div = document.querySelector('div');
-
-// div.addEventListener('click',(e) => {
-//     console.log(e);
-//     e.target.textContent === 'clicked' ?
-//     e.target.textContent = 'title' :
-//     e.target.textContent = 'clicked';
-// });
-// }
-
-const addButton = document.querySelector('#add');
-const newItem = document.querySelector('input');
-const ul = document.querySelector('ul');
-
-console.log(newItem.value);
-
-
-
 /*
  * version 1 remove li element when clicked this runs only for created li elements
  */
-// const items = document.querySelectorAll('li');
-// items.forEach((item) => {
+// const liItems = document.querySelectorAll('li');
+// liItems.forEach( item => {
 //     console.log(item.innerText);
-//     item.addEventListener('click',(event)=> {
+//     item.addEventListener('click',event=> {
 //         console.log(event.target.innerText);
 //         item.remove();
 //     })
@@ -42,23 +14,25 @@ console.log(newItem.value);
 /*
  * add li element when addButton clicked take input from input field
  */
+const addButton = document.querySelector('#add');
+const newItem = document.querySelector('input');
+
 addButton.addEventListener('click',(event)=>{
     const li = document.createElement('li');
     console.log(newItem.value);
 
     li.innerText = newItem.value;
     ul.append(li);
+    newItem.value = '';
 });
 
 /*
  * event bubbling demonstration
  * version 2 remove li element when clicked which takes also newli created li elements
  */
-ul.addEventListener('click', (event) => {
-console.log(event.target.innerText);
-        event.target.remove();
+const ul = document.querySelector('ul');
+
+ul.addEventListener('click', event => {
+    console.log(event.target.innerText);
+    event.target.remove();
 });
-
-
-
-
