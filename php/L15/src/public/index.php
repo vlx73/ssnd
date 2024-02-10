5708cc8cc76c71250fcf6e92a6989addd6544ec2
Vlado Mečiar <vmeciar@buttonsystems.eu>
2024-02-10 15:34:35 +0100

    Added .dist file added service for php backend and Dockerfile. Not tested yet.

---
+++ php/L15/src/public/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<h1>PHP</h1>
+<h2><?php echo 'Hello world!' ?></h2>
+<p>PHP is a popular general-purpose scripting language that is especially suited to web development.</p>
+<p>Fast, flexible and pragmatic, PHP powers everything from your blog to the most popular websites in the world.</p>
+<p>And, of course, PHP is open source.</p>
+<p>For more information and documentation, visit the <a href="https://www.php.net">PHP website</a>.</p>
+</body>
+</html>
+
