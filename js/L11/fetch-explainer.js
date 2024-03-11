7086d0142342becd648ad1c1ba3054ef558ba193
Vlado Mečiar <vmeciar@buttonsystems.eu>
2024-03-11 11:12:33 +0100

    Otestoavný fetchexplainer a promiseexplainer.

--- js/L11/fetch-explainer.js
+++ js/L11/fetch-explainer.js
@@ -6,6 +6,12 @@ fetch('https://jsonplaceholder.typicode.com/users')
     })
     .then(data => {
         console.log(data);
+        const ul = document.querySelector('ul');
+        data.forEach(user => {
+            let li = document.createElement('li');
+            li.textContent = user.name;
+            ul.appendChild(li);
+        })
     })
     .catch(errorData => {
         // iba ak je nemožné spraviť request - DNS, network, ...
