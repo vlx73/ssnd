d4d02a23d8f5a11fa483e3ab822d0b02931de317
Vlado Mečiar <vmeciar@buttonsystems.eu>
2024-11-13 11:49:57 +0100

    Aktualizácia príkladov.

--- js/L7/js/l7.js
+++ js/L7/js/l7.js
@@ -1,15 +1,14 @@
 /*
  * version 1 remove li element when clicked this runs only for created li elements
  */
-// const liItems = document.querySelectorAll('li');
-// liItems.forEach( item => {
-//     console.log(item.innerText);
-//     item.addEventListener('click',event=> {
-//         console.log(event.target.innerText);
-//         item.remove();
-//     })
-// })
-
+const liItems = document.querySelectorAll('li');
+liItems.forEach(item => {
+    console.log(item.innerText);
+    item.addEventListener('click', event => {
+        console.log(event.target.innerText);
+        item.remove();
+    })
+})
 
 /*
  * add li element when addButton clicked take input from input field
@@ -17,12 +16,18 @@
 const addButton = document.querySelector('#add');
 const newItem = document.querySelector('input');
 
-addButton.addEventListener('click',(event)=>{
+addButton.addEventListener('click', (event) => {
+    // vytvoríme nový li element bez jeho umiestnenia v DOM
     const li = document.createElement('li');
     console.log(newItem.value);
 
+    // pridáme text do li elementu
     li.innerText = newItem.value;
+
+    // umiestnime li element do zoznamu ul na koniec
     ul.append(li);
+
+    // vyprádnime input field
     newItem.value = '';
 });
 
@@ -30,9 +35,37 @@ addButton.addEventListener('click',(event)=>{
  * event bubbling demonstration
  * version 2 remove li element when clicked which takes also newli created li elements
  */
-const ul = document.querySelector('ul');
+// const ul = document.querySelector('ul');
+//
+// ul.addEventListener('click', event => {
+//     console.log(event.target.innerText);
+//     event.target.remove();
+// });
 
-ul.addEventListener('click', event => {
-    console.log(event.target.innerText);
-    event.target.remove();
+/**
+ * switch input type between text and password
+ */
+let swichElement = document.querySelector('#switch');
+let passwordInput = document.querySelector('#loginPassword');
+
+swichElement.addEventListener('click', event => {
+
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+    } else {
+        passwordInput.type = 'password';
+    }
+});
+
+passwordInput.addEventListener('input', event => {
+    console.log(event.target.value);
+
+    // check password length and toggle class
+    if (event.target.value.length > 8) {
+        event.target.classList.add('valid');
+        event.target.classList.remove('invalid');
+    } else {
+        event.target.classList.remove('valid');
+        event.target.classList.add('invalid');
+    }
 });
