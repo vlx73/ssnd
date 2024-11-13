84dec5f5ead6526cbb250503d2c4375a98a647fc
Vlado Mečiar <vmeciar@buttonsystems.eu>
2024-11-13 21:54:40 +0100

    Aktualizácia príkladov.

--- js/L10/js/l10.js
+++ js/L10/js/l10.js
@@ -1,20 +1,18 @@
 /*
  * demo for XMLHttpRequest
  */
-
-
 const getUsers = function (callback) {
     const request = new XMLHttpRequest();
 
     request.addEventListener('readystatechange', () => {
-        console.log('REQUEST readySate: ' + request.readyState + ' http status: ' + request.status,request);
+        console.log('REQUEST readySate: ' + request.readyState + ' http status: ' + request.status, request);
 
         if (request.readyState === 4 && request.status === 200) {
-
+            console.log('Serialized data (JSON): ', request.responseText);
             let responseData = JSON.parse(request.responseText);
             callback(responseData);
         } else {
-            console.log('IN PROGRESS');
+            console.log('Not done yet');
         }
     });
 
@@ -23,17 +21,23 @@ const getUsers = function (callback) {
 }
 
 /*
- * demo for fetch
+ * Volanie funkcie getUsers s callbackom
  */
-getUsers((responseData) => {
-    console.log('RESPONSE data: ', responseData);
 
-    list = document.querySelector('ul');
+// listener for load button
+const loadButton = document.querySelector('#load');
+loadButton.addEventListener('click', event => {
+    getUsers(responseData => {
+        console.log('RESPONSE data: ', responseData);
 
-    // add new li to list
-    responseData.forEach(element => {
-        newLi = `<li>${element.address.street}</li>`
-        console.log(newLi);
-        list.innerHTML += (newLi);
+        list = document.querySelector('ul');
+
+        // add new li to list
+        responseData.forEach(element => {
+            newLi = `<li>${element.address.street}</li>`
+            console.log(newLi);
+            list.innerHTML += (newLi);
+        });
     });
 });
+
