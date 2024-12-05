c8d22570975188c90d7f7df69bfc3de7c52a8962
Vlado Mečiar <vmeciar@buttonsystems.eu>
2024-12-05 07:56:38 +0100

    Cleanup

--- js/L11/callback.js
+++ js/L11/callback.js
@@ -7,7 +7,7 @@ const getRemoteData = function (endpoint, callback) {
 
     request.addEventListener('readystatechange', () => {
         // console.log('REQUEST', request.readyState, request.status, request)
-        if (request.readyState == 4 && request.status == 200) {
+        if (request.readyState === 4 && request.status === 200) {
             console.log('Odpoveď OK: ', request);
             let responseData = JSON.parse(request.responseText);
 //            console.log(responseData);
@@ -15,7 +15,7 @@ const getRemoteData = function (endpoint, callback) {
             // // volanie callback funkcie
             callback(undefined, responseData);
 
-        } else if (request.readyState == 4) {
+        } else if (request.readyState === 4) {
             console.log('Odpoveď: ', request);
 
             callback('Status NOK', undefined);
@@ -33,47 +33,39 @@ getRemoteData('https://jsonplaceholder.typicode.com/users', (error, responseData
     if (error) {
         console.log(error);
     } else {
-        console.log(responseData);
         list = document.querySelector('#users');
-        console.log(list);
 
+        // render user data
         responseData.forEach(element => {
             newLi = `<li>User: <h2>${element.address.street}</h2><ul id="user-${element.id}"></ul></li>`
-            //console.log(newLi);
             list.innerHTML += (newLi);
         });
 
+        // get albums
         getRemoteData('https://jsonplaceholder.typicode.com/albums', (error, responseData) => {
             if (error) {
                 console.log(error);
             } else {
-                //console.log('RESPONSE', responseData);
-
+                // render album data
                 responseData.forEach(element => {
                     newLi = `<li>Album: <h3>${element.title}</h3><ul id="album-${element.id}"></ul></li>`
-                    //                    console.log(newLi);
 
                     userListId = '#user-' + element.userId;
-                    // console.log(userListId);
-
                     document.querySelector(userListId).innerHTML += (newLi);
                 });
             }
         });
 
+        // get photos
         getRemoteData('https://jsonplaceholder.typicode.com/photos', (error, responseData) => {
 
             if (error) {
                 console.log(error);
             } else {
-                //  console.log('RESPONSE', responseData);
-
+                // render photo data
                 responseData.forEach(element => {
                     newLi = `<li>${element.title}<img src="${element.thumbnailUrl}" /></li>`;
-                    // console.log(newLi);
-
                     albumlistId = '#album-' + element.albumId;
-                    //  console.log(albumlistId);
 
                     document.querySelector(albumlistId).innerHTML += (newLi);
                 });
