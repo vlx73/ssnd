7181e6b63e2ca9ad637b51cf02e17843c70e5330
Vlado Mečiar <vmeciar@buttonsystems.eu>
2024-12-19 06:09:14 +0100

    Ošetrený http status.

--- js/L10/fetch-explainer.js
+++ js/L10/fetch-explainer.js
@@ -1,11 +1,18 @@
-
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => {
         // dostali sme data ale ešte sú vo formáte RESPONSE
         console.log('Resolved: ', response);
 
+        // musíme si ošetreni HTTP status
+        console.log('HTTP response status: '.response.status);
+
         // prevedieme response na JSON čo je opäť asynchrónna operácia
-        return response.json();
+        if (response.status === 200) {
+            return response.json();
+        } else {
+            // ak je status iný ako 200, tak vyvoláme chybu
+            throw new Error('Unable to fetch data');
+        }
     })
     .then(data => {
         console.log(data);
