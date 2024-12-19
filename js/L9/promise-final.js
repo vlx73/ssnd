50af7fd17e801be983fc95cd8e178d4eab55573a
Vlado Mečiar <vmeciar@buttonsystems.eu>
2024-12-19 05:20:58 +0100

    Fixnuté demo pre porovnanie s call back riešením single aj final

--- js/L9/promise-final.js
+++ js/L9/promise-final.js
@@ -1,57 +1,116 @@
-/*
-* callbacks
-*/
-const getRemoteData = function (sourceURL) {
+/**
+ * Funkcia na získanie dát z vzdialeného zdroja použitím Promise
+ *
+ * @param sourceURL
+ * @param callback
+ * @returns {Promise<unknown>}
+ */
+const getRemoteData = function (sourceURL, callback) {
 
-    return new Promise((resolve, reject) => {
+    return new Promise((resolveFunction, rejectFunction) => {
         const request = new XMLHttpRequest();
 
         request.addEventListener('readystatechange', () => {
-            //console.log('REQUEST', request.readyState, request.status, request)
-            if (request.readyState == 4 && request.status == 200) {
-                //console.log(request.getAllResponseHeaders());
+
+            if (request.readyState === 4 && request.status === 200) {
                 let responseData = JSON.parse(request.responseText);
 
-                // volanie callback funkcie
-                resolve(responseData);
+                // callback pre stav OK
+                resolveFunction(responseData);
+            } else if (request.readyState === 4) {
 
-            } else if (request.readyState == 4) {
-                reject('Status NOK');
-                //    console.log('IN PROGRESS');
+                // callback pre stav NOT OK
+                rejectFunction('Nieč nezahralo ako sme čakali');
             }
         });
 
         request.open('GET', sourceURL);
         request.send();
     });
-
-
 }
 
 getRemoteData('https://jsonplaceholder.typicode.com/users')
     .then(responseData => {
-        list = document.querySelector('#users');
-        responseData.forEach(element => {
-            newLi = `<li>User: <h2>${element.address.street}</h2><ul id="user-${element.id}"></ul></li>`
-            list.innerHTML += (newLi);
+        const userUl = document.querySelector('#users');
+
+        // responseData je pole objektov user
+        responseData.forEach(user => {
+            let newLi = `<li>User: <h2>${user.address.street}</h2><ul id="user-${user.id}"></ul></li>`
+            userUl.innerHTML += (newLi);
         });
 
+        // po načítaní userov načítame ďalšie dáta - albumy - vracia mi to opäť Promise
         return getRemoteData('https://jsonplaceholder.typicode.com/albums');
-    }).then(responseData => {
-        responseData.forEach(element => {
-            newLi = `<li>Album: <h3>${element.title}</h3><ul id="album-${element.id}"></ul></li>`
-            userListId = '#user-' + element.userId;
+    })
+    .then(responseData => {
+
+        // responseData je pole objektov album
+        responseData.forEach(album => {
+            let newLi = `<li>Album: <h3>${album.title}</h3><ul id="album-${album.id}"></ul></li>`
+            let userListId = '#user-' + album.userId;
             document.querySelector(userListId).innerHTML += (newLi);
         });
 
+        // po načítaní albumov načítame ďalšie dáta - fotky - vracia mi to opäť Promise
         return getRemoteData('https://jsonplaceholder.typicode.com/photos');
-    }).then(responseData => {
-        responseData.forEach(element => {
-            newLi = `<li>${element.title}<img src="${element.thumbnailUrl}" /></li>`;
-            albumlistId = '#album-' + element.albumId;
+    })
+    .then(responseData => {
+
+        // responseData je pole objektov photo
+        responseData.forEach(photo => {
+            let newLi = `<li>${photo.title}<img src="${photo.thumbnailUrl}" /></li>`;
+            let albumlistId = '#album-' + photo.albumId;
             document.querySelector(albumlistId).innerHTML += (newLi);
         });
     })
     .catch((error) => {
         console.log(error);
     });
+
+/**
+ * Takto to vyzeralo predtým
+ */
+// getRemoteData('https://jsonplaceholder.typicode.com/users', (error, responseData) => {
+//
+//     if (error) {
+//         console.log(responseData);
+//     } else {
+//         const usersUl = document.querySelector('#users');
+//
+//         // render user data
+//         responseData.forEach(user => {
+//             let newLi = `<li>User: <h2>${user.address.street}</h2><ul id="user-${user.id}"></ul></li>`
+//             usersUl.innerHTML += (newLi);
+//         });
+//
+//         // level 2 - get albums
+//         getRemoteData('https://jsonplaceholder.typicode.com/albums', (error, responseData) => {
+//             if (error) {
+//                 console.log(responseData);
+//             } else {
+//                 // render album data
+//                 responseData.forEach(album => {
+//                     let newLi = `<li>Album: <h3>${album.title}</h3><ul id="album-${album.id}"></ul></li>`
+//                     let userListId = '#user-' + album.userId;
+//
+//                     document.querySelector(userListId).innerHTML += (newLi);
+//                 });
+//
+//                 // level 3 - get photos
+//                 getRemoteData('https://jsonplaceholder.typicode.com/photos', (error, responseData) => {
+//
+//                     if (error) {
+//                         console.log(responseData);
+//                     } else {
+//                         // render photo data
+//                         responseData.forEach(photo => {
+//                             let newLi = `<li>${photo.title}<img src="${photo.thumbnailUrl}" /></li>`;
+//                             let albumlistId = '#album-' + photo.albumId;
+//                             document.querySelector(albumlistId).innerHTML += (newLi);
+//                         });
+//                     }
+//                 });
+//             }
+//         });
+//     }
+// });
